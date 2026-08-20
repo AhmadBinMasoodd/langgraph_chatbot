88715593df8caa600863593148ae1ecc0daf9f99
Ahmad Bin Masoodd <ahmabinmasoodd@gmail.com>
2026-08-20 11:49:34 +0500

    add the functionality of streaming

--- frontend.py
+++ frontend.py
@@ -18,10 +18,16 @@ if user_input:
     with st.chat_message('user'):
         st.text(user_input)
 
-    # invoke the chatbot here
-    response=chatbot.invoke({'messages':[HumanMessage(content=user_input)]},config=CONFIG)
-    ai_message=response['messages'][-1].content
-
-    st.session_state['messages_history'].append({'role':'assistant','content':ai_message})
+    
+    # st.session_state['messages_history'].append({'role':'assistant','content':ai_message})
     with st.chat_message('assistant'):
+        ai_message=st.write_stream(
+            message_chunk.content for message_chunk,metadata in chatbot.stream(
+                {
+                    'messages':[HumanMessage(content=user_input)],
+                },
+                config=CONFIG,
+                stream_mode='messages'
+            )
+        )
         st.text(ai_message)
