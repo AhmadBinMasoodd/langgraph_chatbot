fe4cc80fb6e3a9005ee207949cf649e49200f700
Ahmad Bin Masoodd <ahmabinmasoodd@gmail.com>
2026-08-21 12:06:51 +0500

    obervability using the langsmith

--- frontend.py
+++ frontend.py
@@ -42,7 +42,13 @@ if 'chat_threads' not in st.session_state:
     st.session_state['chat_threads']=retrieve_all_threads()
 
 add_thread(st.session_state['thread_id'])
-CONFIG={'configurable':{'thread_id':st.session_state['thread_id']}}
+CONFIG={
+    'configurable':{'thread_id':st.session_state['thread_id']},
+    'metadata': {
+        "thread_id":st.session_state['thread_id']
+    },
+    'run_name':'chat_turn'
+}
 
 #   ---------------- sidebar ui
 st.sidebar.title('LangGraph Chatbot')
